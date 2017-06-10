a0a664d37db07dcdb3950bb7a1191df1b98d91be
Ravi Bhikhie <r.bhikhie@gmail.com>
2017-06-10 03:16:12 +0200

    find a solution, partial script.

--- p004.py
+++ p004.py
@@ -6,8 +6,6 @@
 # The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-import math
-
 # Upper bound for numbers = 999
 # Lower bound for numbers = 100
 # max_product = 998001
@@ -26,7 +24,23 @@ for i,v in enumerate(factor_1):
 
     for j,w in enumerate(factor_2):
         f2 = w
-        test_answer = f1*f2
-        print(test_answer)
+        test_ans_num = (f1*f2)
+        test_answer = str(f1*f2)
 
-exit("Ending script.")
+        if (test_ans_num % 2 == 0):
+            left = test_answer[0:3]
+            right = test_answer[3:]
+            right_mir = right[::-1]
+
+            if left == right_mir:
+                print(test_ans_num)
+                exit("Answer found.")
+
+        elif(test_ans_num % 2 != 0):
+            left = test_answer[0:2]
+            right = test_answer[3:]
+
+        else:
+            pass
+
+exit("Ending script, no solution found.")
