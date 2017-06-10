44982a0ba9fda3dd3de74c5ade3fcd06eedeb7bf
Ravi Bhikhie <r.bhikhie@gmail.com>
2017-06-11 01:57:14 +0200

    Create alternate solution: work backwards

---
+++ p004/p004-alt1.py
@@ -0,0 +1,15 @@
+# Ravi Bhikhie
+# Euler Project
+
+# Problem 004
+# A palindromic number reads the same both ways.
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+# Upper bound for numbers = 999
+# Lower bound for numbers = 100
+# max_product = 998001
+# min_product = 10000
+#
+# This is to approach from the other way: Create a list of all possible solutions and evaluate it backwards.
+#
