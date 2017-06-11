2edf8427c7bb8ef67034cd130223cb9b955e42ba
Ravi Bhikhie <r.bhikhie@gmail.com>
2017-06-11 15:23:41 +0200

    clean up problem 5

--- p005/p005_01.py
+++ p005/p005_01.py
@@ -1,6 +1,6 @@
 # Ravi Bhikhie
 # Euler Project
-# Problem 006
+# Problem 005
 
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
@@ -8,9 +8,8 @@
 # Assumption: prime numbers are the most relevant factor that drive up this number.
 
 # Code is not needed.
-# prime numbers under 20:
-# 3, 5, 7, 11, 13, 17, 19
-# Other factors(2, 4, 6, 8, 10, 12, 14, 18) are contained within 9 and 16.
+# Relevant prime numbers under 20:
+# 5, 7, 11, 13, 17, 19
+# Other factors(2, 3, 4, 6, 8, 9, 10, 12, 14, 16 and 18) are contained within the factors of 9 and 16.
 
 print(5*7*9*11*13*16*17*19)
-
