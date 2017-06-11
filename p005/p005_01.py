3accbca464cac244fd6e30c4f077f8990e2477e1
Ravi Bhikhie <r.bhikhie@gmail.com>
2017-06-11 15:18:01 +0200

    rework and print solution for 5, init problem 6

--- p005/p005_01.py
+++ p005/p005_01.py
@@ -4,6 +4,13 @@
 
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+# ----------------------------
+# Assumption: prime numbers are the most relevant factor that drive up this number.
 
+# Code is not needed.
+# prime numbers under 20:
+# 3, 5, 7, 11, 13, 17, 19
+# Other factors(2, 4, 6, 8, 10, 12, 14, 18) are contained within 9 and 16.
 
-exit()
+print(5*7*9*11*13*16*17*19)
+
