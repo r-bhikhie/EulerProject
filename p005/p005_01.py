81f5f7d95c3b0dd067250e52a7b128a4e319f6f6
Ravi Bhikhie <r.bhikhie@gmail.com>
2017-06-11 15:19:33 +0200

    work on problem 6

--- p005/p005_01.py
+++ p005/p005_01.py
@@ -1,6 +1,6 @@
 # Ravi Bhikhie
 # Euler Project
-# Problem 001
+# Problem 006
 
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
